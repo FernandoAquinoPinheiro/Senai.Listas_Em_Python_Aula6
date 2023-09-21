99e084d1ae221c95535f1b53bce76d8126c35ae4
FernandoAquinoPinheiro <144848529+FernandoAquinoPinheiro@users.noreply.github.com>
2023-09-21 15:16:09 -0300

    add_files

---
+++ main.py
@@ -0,0 +1,90 @@
+# identação é a organização dos codigos
+# isso é uma variavel
+# nome = 'camila"
+# 7 indices
+# variedades de Dados menos digitação
+# estrutura composta
+# indices == 0 e o valores 
+# lista = [1,3.28,-6,5,5, "casa",2.8,True ]
+# lista.append(2)
+# print(lista)
+
+# ----------------------------------
+
+
+# lista2 = [25,3,566,545,5678,25]
+# lista2.remove(25)
+# print(lista2)
+
+# del lista2[0]
+# print(lista2)
+
+# lista4 = len(lista2)
+# print(lista4)
+
+# ---------------------------------
+
+# lista5 = [1,3,45,89,78,123,45,78,56]
+# for i in lista5:
+#   print(i)
+
+# ---------------------------
+
+# Exercicios
+
+# 1 -Exercício 1: Crie uma lista chamada numeros que contenha os números inteiros de 1 a 5 e imprima-a na tela.
+
+# numeros = [1,6,7,10,20]
+# print(numeros)
+
+
+
+# 2: Acesse e imprima o terceiro elemento da lista numeros.
+
+# numeros = [1,6,7,10,20]
+# print(numeros[2])
+
+#  3: Adicione o número 6 à lista numeros e imprima a lista atualizada.
+
+# numeros = [1,4,7,10,20]
+# numeros.append(6)
+# print(numeros)
+
+# 4: Remova o número 3 da lista numeros e imprima a lista resultante.
+
+# numeros = [1,2,3,7,10,20]
+# numeros.remove(3)
+# print(numeros)
+
+# 5: Crie uma lista chamada frutas contendo três nomes de frutas diferentes. Em seguida, concatene essa lista com a lista numeros e imprima o resultado.
+
+# numeros = [1,2,3,7,10,20]
+# frutas = ["pera","maça","melancia"]
+# num_fru = numeros + frutas
+# print(num_fru)
+
+# 6: Use um loop for para percorrer e imprimir todos os elementos da lista frutas.
+
+# frutas = ["pera","maça","melancia"]
+# for i in frutas:
+#   print(i)
+
+# 7: Verifique se o número 5 está presente na lista numeros e imprima uma mensagem informando se ele está na lista ou não.
+
+# numeros = [1,6,7,10,20]
+# if 5 in numeros:
+#   print("O numero 5 esta na lista")
+# else:
+#  print("O numero 5 não esta na lista")
+
+
+
+
+
+
+
+
+
+
+
+
